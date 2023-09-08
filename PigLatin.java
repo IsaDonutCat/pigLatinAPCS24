0d67f55c8e7cf54c12fb48b18bd9b5507c154b18
Isabella Wang <wangisa26@7hills.org>
2023-09-08 13:15:06 -0400

    idk whats happening so i'm restarting like it's cs50 again

--- PigLatin.java
+++ PigLatin.java
@@ -8,36 +8,18 @@ public class PigLatin
 
         System.out.print("Original:");
         String phrase = input.nextLine();
-
+        String word;
         int len = phrase.length();
-        int start = 0;
-        String chara;
-        int cycle; // intended to cycle through consonants until it reaches a value;
 
         for (int i = 0; i < len; i++)
-        {
-            if (phrase.substring(i, i + 1) == " "); // up to but not including. phrase[i] is a space
-            {   
-                chara = phrase.substring(start, start + 1);
-
-                if (chara == "a" || chara == "e" || chara == "i" || chara == "o" || chara == "u") // add way if word starts with a vowel
-                {
-                    phrase = phrase.substring(0, i) + "way" + phrase.substring(i, len);
-                }
-                else
-                {   
-                    cycle = start + 1; // starts on the 2nd letter of a word
-                    while (!(chara == "a" || chara == "e" || chara == "i" || chara == "o" || chara == "u"))
-                    {
-                        chara = phrase.substring(cycle, cycle + 1); 
-                        cycle += 1;
-                    }
-
-                    phrase = phrase.substring(0 , start) + phrase.substring(cycle - 1, cycle) + phrase.substring(start, cycle - 1) + "ay" + phrase.substring(i, len);
-                }
-                start = i + 1;
+        {   
+            if (phrase[i] == " ")
+            {
+                word = phrase.substring("");
             }
+            //TODO: does the ph
         }
 
+        System.out.println(phrase);
     }
 }

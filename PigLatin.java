e5043a4157cf43ee49032886685a63c930932991
Isabella Wang <wangisa26@7hills.org>
2023-09-08 13:05:07 -0400

    trying to get consonant rule of pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -12,13 +12,27 @@ public class PigLatin
         int len = phrase.Length();
         int start = 0;
         String chara;
+        int cycle; // intended to cycle through consonants until it reaches a value;
 
         for (int i = 0; i < len; i++)
         {
             if (phrase.substring(i, i + 1) == " "); // up to but not including. phrase[i] is a space
             {   
                 chara = phrase.substring(start, start + 1);
-                if (phrase.substring(start, start + 1) == )
+
+                if (chara == "a" || chara == "e" || chara == "i" || chara == "o" || chara == "u") // add way if word starts with a vowel
+                {
+                    phrase = phrase.substring(0, i) + "way" + phrase.substring(i, len);
+                }
+                else
+                {   
+                    cycle = start + 1;
+                    while (!(chara == "a" || chara == "e" || chara == "i" || chara == "o" || chara == "u"))
+                    {
+                        chara = phrase.substring(cycle, cycle + 1);
+                        cycle += 1;
+                    }
+                }
                 start = i + 1;
             }
         }

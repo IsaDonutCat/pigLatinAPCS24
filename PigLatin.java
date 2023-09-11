3018d5e91530d51a143578e75f49ac408bdc8572
Isabella Wang <wangisa26@7hills.org>
2023-09-11 15:56:21 -0400

    updated for last word in a string

--- PigLatin.java
+++ PigLatin.java
@@ -42,6 +42,31 @@ public class PigLatin
                     start = i + 1;
                 }
             }
+            else if (i == (len - 1))
+            {
+                if (phrase[start] == 'a' || phrase[start] == 'e' || phrase[start] == 'i' || phrase[start] == 'o' || phrase[start] == 'u') //vowel at the start of the word
+                {
+                    origin = origin.substring(0, i) + "way" + origin.substring(i, len);
+                    phrase = origin.toCharArray(); //updates both char array and origin
+                    len = origin.length(); //modifies len so it remembers to cycle through the rest of the word
+                    i += 3; //modifies i so it doesn't check a word that has already been pig-latined
+                    start = i + 1; //sets the start to the next word, assuming here is only one space between words;
+                }
+                else
+                {
+                    difference = start; 
+                    while (phrase[difference] != 'a' || phrase[difference] != 'e' || phrase[difference] != 'i' || phrase[difference] != 'o' || phrase[difference] != 'u')
+                    {
+                        difference += 1;
+                    } //quits loop when phrase[difference] is a vowel
+
+                    origin = origin.substring(0, start) + origin.substring(difference, i) + origin.substring(start, difference) + "ay" + origin.substring(i, len);
+                    phrase = origin.toCharArray();
+                    len = origin.length();
+                    i += 2;
+                    start = i + 1;
+                }
+            }
         }
 
         System.out.println(phrase);

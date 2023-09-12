54390fb6366b8a387b35f269e224168d13fd2b6e
Isabella Wang <wangisa26@7hills.org>
2023-09-12 12:16:04 -0400

    created arraylist of words strings to prepare for pig latinating

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Character;
+import java.util.ArrayList;
 
 public class PigLatin
 {
@@ -9,10 +10,11 @@ public class PigLatin
 
         System.out.print("Original:");
         String origin = input.nextLine();
+        ArrayList<String> wordsList = new ArrayList<String>();
         char[] phrase = origin.toCharArray();
         int len = origin.length();
         int spaceCount = 0;
-        int previouSpace;
+        int previouSpace = 0;;
 
         for (int i = 0; i < len; i++)
         {
@@ -22,12 +24,28 @@ public class PigLatin
                 if (spaceCount == 1)
                 {
                     previouSpace = i;
+                    wordsList.add(origin.substring(0, i));
                 }
-            }
+                else
+                {
+                    wordsList.add(origin.substring(previouSpace + 1, i));
+                    previouSpace = i;
+                }
+            }   
 
+            if (i == len - 1)
+            {
+                //System.out.println("last end reached fo string");
+                wordsList.add(origin.substring(previouSpace + 1, len));
+            }
         }
 
-        System.out.println(phrase);
+        /*for(int i = 0; i < wordsList.size(); i++) 
+        {   
+            System.out.println(wordsList.get(i));
+        }*/
+
+        //System.out.println(phrase);
         input.close();
     }
 }

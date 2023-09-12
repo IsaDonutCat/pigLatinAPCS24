c833c30cf80457b478c6d5442fe6c85aa2f3ace8
Isabella Wang <wangisa26@7hills.org>
2023-09-12 12:22:07 -0400

    starting the pig latinator

--- PigLatin.java
+++ PigLatin.java
@@ -9,12 +9,13 @@ public class PigLatin
         Scanner input = new Scanner(System.in);
 
         System.out.print("Original:");
-        String origin = input.nextLine();
-        ArrayList<String> wordsList = new ArrayList<String>();
-        char[] phrase = origin.toCharArray();
-        int len = origin.length();
-        int spaceCount = 0;
-        int previouSpace = 0;;
+
+        String origin = input.nextLine(); //gets input
+        ArrayList<String> wordsList = new ArrayList<String>(); //intializes an arraylist,which is a like a dynamic memory from c
+        int len = origin.length(); //gets len to run through and find spaces
+        
+        int spaceCount = 0; //finds spaces to make sure it's the first space so words can be accurately stored
+        int previouSpace = 0;;  //stores prev space to make sure that the words are correctly stored
 
         for (int i = 0; i < len; i++)
         {
@@ -38,13 +39,21 @@ public class PigLatin
                 //System.out.println("last end reached fo string");
                 wordsList.add(origin.substring(previouSpace + 1, len));
             }
-        }
+        } //creates an array of words to pig latinate
 
         /*for(int i = 0; i < wordsList.size(); i++) 
         {   
             System.out.println(wordsList.get(i));
         }*/
 
+        int wordCt = wordsList.size();
+        char[] word;
+
+        for (int i = 0; i < wordCt; i ++)
+        {
+            word = wordsList.get(i);
+            
+        }
         //System.out.println(phrase);
         input.close();
     }

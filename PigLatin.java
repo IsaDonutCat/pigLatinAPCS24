e4623222b894c9bde0e7d3152d8bfdfc553bcb1f
Isabella Wang <wangisa26@7hills.org>
2023-10-19 15:26:48 -0400

    trying to javadoc shortcut

--- PigLatin.java
+++ PigLatin.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 
 public class PigLatin
 {
+    
+    /** 
+     * @param args
+     */
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
